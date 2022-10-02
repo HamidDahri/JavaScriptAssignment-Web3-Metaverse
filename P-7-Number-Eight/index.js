29196f174ed71b180073d9576c13fa1f521789e4
Hamid Mustafa Dahri <56677019+HamidDahri@users.noreply.github.com>
2022-10-02 22:18:14 +0500

    7 & 8 completed

---
+++ P-7-Number-Eight/index.js
@@ -0,0 +1,22 @@
+
+
+const addition = (n1,n2) => {
+
+    console.log(n1+n2);
+}
+
+const subtraction = (n1,n2) => {
+    console.log(n1-n2);
+}
+
+const multiplication = (n1,n2) => {
+    console.log(n1*n2);
+}
+const division = (n1,n2) => {
+    console.log(n1/n2);
+}
+
+addition(4,4);
+subtraction(16,8);
+multiplication(4,2);
+division(64,8);
